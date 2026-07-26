e9987624d45ce76bcfe58f28bc18c495d487dc12
gargeeshirodkar61-max <gargeeshirodkar61@gmail.com>
2026-07-27 00:12:51 +0530

    these are my login modules files

---
+++ LOGIN_MODULE1/login1.java
@@ -0,0 +1,8 @@
+package PROJECT_DEMO1.LOGIN_MODULE1;
+
+public class login1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
---
+++ LOGIN_MODULE1/login2.java
@@ -0,0 +1,8 @@
+package PROJECT_DEMO1.LOGIN_MODULE1;
+
+public class login2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Guys Welcome to the world of Java Super_x_project");
+    }
+    
+}
