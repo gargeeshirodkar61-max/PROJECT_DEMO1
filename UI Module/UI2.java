e7c8bdd1a58ae66d4d6251ab2215137aebde7ed5
kalyaniupare06-collab <kalyaniupare06@gmail.com>
2026-07-27 00:17:51 +0530

    Changes 1

---
+++ UI Module/UI2.java
@@ -0,0 +1,17 @@
+
+
+public class UI2 {
+    void display() {
+        System.out.println("UI2 is displayed.");
+    }
+
+    UI2(){
+        System.out.println("UI2 is initialized.");
+    }
+
+    public static void main(String[] args) {
+        UI2 ui2 = new UI2();
+        ui2.display();
+    }
+    
+}
